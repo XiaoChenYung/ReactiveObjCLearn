42e19d62317138f9ffd61d4cb1544e2f0e60e2ee
杨晓晨 <xiaochenyung@gmail.com>
2016-11-18 14:50:17 +0800

    success

--- ReactiveObjCLearnt/ViewController.m
+++ ReactiveObjCLearnt/ViewController.m
@@ -7,18 +7,25 @@
 //
 
 #import "ViewController.h"
-
+#import <ReactiveObjC/ReactiveObjC.h>
 @interface ViewController ()
-
+@property (copy, nonatomic) NSString *window;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.window = @"w2r";
+    [RACObserve(self, window) subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@",x);
+    }];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.window = @"asdf";
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
