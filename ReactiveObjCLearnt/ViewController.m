550b9400f37263ad5a37a05581fb276eaf9c659d
杨晓晨 <xiaochenyung@gmail.com>
2016-11-18 15:59:29 +0800

    command

--- ReactiveObjCLearnt/ViewController.m
+++ ReactiveObjCLearnt/ViewController.m
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *psd;
 @property (weak, nonatomic) IBOutlet UIButton *okBtn;
 @property (copy, nonatomic) NSString *window;
+@property (strong, nonatomic) RACCommand *loginCommand;
 @end
 
 @implementation ViewController
@@ -20,9 +21,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    RAC(self.okBtn, hidden) = [RACSignal combineLatest:@[RACObserve(self, uname.text), RACObserve(self, psd.text)] reduce:^(NSString *uname, NSString *psd){
-        return @([uname isEqualToString:psd]);
+    self.loginCommand = [[RACCommand alloc] initWithSignalBlock:^(id sender) {
+        return [self loginIn];
     }];
+    
+    [self.loginCommand.executionSignals subscribeNext:^(RACSignal *loginSignal) {
+        [loginSignal subscribeCompleted:^{
+            NSLog(@"done");
+        }];
+    }];
+    
+    self.okBtn.rac_command = self.loginCommand;
+    
+//    self.okBtn.rac_command = [[RACCommand alloc] initWithSignalBlock:^RACSignal * _Nonnull(id  _Nullable input) {
+//        NSLog(@"sdafgas");
+//        return [RACSignal empty];
+//    }];
+    
+//    RAC(self.okBtn, hidden) = [RACSignal combineLatest:@[RACObserve(self, uname.text), RACObserve(self, psd.text)] reduce:^(NSString *uname, NSString *psd){
+//        return @([uname isEqualToString:psd]);
+//    }];
     
 //    self.window = @"w2r";
 //    [[RACObserve(self, window) filter:^BOOL(NSString *value) {
@@ -31,6 +49,10 @@
 //        NSLog(@"%@",x);
 //    }];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (RACSignal *)loginIn {
+    return [RACSignal empty];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
