8be3b0cf0e2f4d9f1edce1405ca3a7e5f107efda
杨晓晨 <xiaochenyung@gmail.com>
2016-11-20 10:07:17 +0800

    signal

--- ReactiveObjCLearnt/ViewController.m
+++ ReactiveObjCLearnt/ViewController.m
@@ -20,35 +20,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.loginCommand = [[RACCommand alloc] initWithSignalBlock:^(id sender) {
-        return [self loginIn];
-    }];
-    
-    [self.loginCommand.executionSignals subscribeNext:^(RACSignal *loginSignal) {
-        [loginSignal subscribeCompleted:^{
+    [self signal];
+}
+
+- (void)signal {
+    RACSignal *signal = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        [subscriber sendNext:@"sss"];
+        
+        return [RACDisposable disposableWithBlock:^{
             NSLog(@"done");
         }];
     }];
     
-    self.okBtn.rac_command = self.loginCommand;
     
-//    self.okBtn.rac_command = [[RACCommand alloc] initWithSignalBlock:^RACSignal * _Nonnull(id  _Nullable input) {
-//        NSLog(@"sdafgas");
-//        return [RACSignal empty];
-//    }];
+    RACDisposable *disposable = [signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@",x);
+    }];
     
-//    RAC(self.okBtn, hidden) = [RACSignal combineLatest:@[RACObserve(self, uname.text), RACObserve(self, psd.text)] reduce:^(NSString *uname, NSString *psd){
-//        return @([uname isEqualToString:psd]);
-//    }];
+    [disposable dispose];
     
-//    self.window = @"w2r";
-//    [[RACObserve(self, window) filter:^BOOL(NSString *value) {
-//        return [value hasPrefix:@"b"];
-//    }] subscribeNext:^(id  _Nullable x) {
-//        NSLog(@"%@",x);
-//    }];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (RACSignal *)loginIn {
