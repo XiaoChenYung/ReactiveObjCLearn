0990f1f9b8531253f15ef6357de69dfbd0354222
杨晓晨 <xiaochenyung@gmail.com>
2016-12-16 11:23:40 +0800

    collect

--- RACTest/RACTest/ViewController.m
+++ RACTest/RACTest/ViewController.m
@@ -17,23 +17,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSArray *array = @[@1,@2,@3];
-    RACSequence *stream = [array rac_sequence];
-    [[stream map:^NSString *(NSNumber *num) {
-        return [num stringValue];
-    }] foldLeftWithStart:@"" reduce:^id _Nullable(id  _Nullable accumulator, id  _Nullable value) {
-        return [accumulator stringByAppendingString:value];
-    }];
     
-    NSLog(@"%@",[stream array]);
     
-    RACSequence *stream2 = [[array rac_sequence] filter:^BOOL(id  _Nullable value) {
-        return [value integerValue] % 2 == 0;
-    }];
+    [self testCollectSignalsAndCombineLatestOrZip];
     
-    NSLog(@"%@",[stream2 array]);
+    
+//    NSArray *array = @[@1,@2,@3];
+//    RACSequence *stream = [array rac_sequence];
+//    [[stream map:^NSString *(NSNumber *num) {
+//        return [num stringValue];
+//    }] foldLeftWithStart:@"" reduce:^id _Nullable(id  _Nullable accumulator, id  _Nullable value) {
+//        return [accumulator stringByAppendingString:value];
+//    }];
+//    
+//    NSLog(@"%@",[stream array]);
+//    
+//    RACSequence *stream2 = [[array rac_sequence] filter:^BOOL(id  _Nullable value) {
+//        return [value integerValue] % 2 == 0;
+//    }];
+//    
+//    NSLog(@"%@",[stream2 array]);
     
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)testCollectSignalsAndCombineLatestOrZip {
+    //1
+    RACSignal *numbers = @[@(0), @(1), @(2)].rac_sequence.signal;
+    
+    RACSignal *letters1 = @[@"A", @"B", @"C"].rac_sequence.signal;
+    RACSignal *letters2 = @[@"X", @"Y", @"Z"].rac_sequence.signal;
+    RACSignal *letters3 = @[@"M", @"N"].rac_sequence.signal;
+    NSArray *arrayOfSignal = @[letters1, letters2, letters3]; //2
+    
+    
+    [[numbers
+       map:^id(NSNumber *n) {
+           //3
+           return arrayOfSignal[n.integerValue];
+       }]  //4
+     subscribeNext:^(NSArray *array) {
+         NSLog(@"%@, %@", [array class], array);
+     } completed:^{
+         NSLog(@"completed");
+     }];
 }
 
 
